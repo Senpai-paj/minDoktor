aa9a1ec01f57faf3de56e40275d69e9e95916596
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2025-09-30 19:46:52 +0200

    Integrate todo-app as part of root repo by removing nested .git

--- todo-back/types.ts
+++ todo-back/types.ts
@@ -2,7 +2,8 @@ export type Task = {
   id: string; 
   title: string;
   description: string;
-  dueDate: string;
-  createDate: string;
+  dueDate: Date;
+  createDate: Date;
   editDate?: Date;
+  status: boolean;
 }
